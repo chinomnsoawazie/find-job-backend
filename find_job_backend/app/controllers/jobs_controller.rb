ab771dfdc111a6fb3317f47f10ce9c8e70d1d690
awazienommy <coawazie@gmail.com>
2020-02-19 20:48:01 -0500

    notes done

--- find_job_backend/app/controllers/jobs_controller.rb
+++ find_job_backend/app/controllers/jobs_controller.rb
@@ -14,10 +14,10 @@ class JobsController < ApplicationController
     end
     
     def create
-        job = Job.create(job_params)
-        allJobs = Job.all
+      job = Job.create(job_params)
+      allUserJobs = User.all.find(params[:user_id]).jobs
         if job.valid?
-          render json: {job: job, allJobs: allJobs}
+          render json: {job: job, allUserJobs: allUserJobs}
         else
           render json:{errors: job.errors.full_messages}
         end
@@ -25,9 +25,9 @@ class JobsController < ApplicationController
     
     def update
         job = Job.find(params[:id])
-        allJobs = Job.all
+        allUserJobs = User.find(params[:user_id]).jobs
         if job.update(job_params)
-          render json: {job: job, allJobs: allJobs}
+          render json: {job: job, allUserJobs: allUserJobs}
         else
           render json: job.errors, status: :unprocessable_entity
         end
@@ -35,11 +35,14 @@ class JobsController < ApplicationController
     
     def destroy
         Job.destroy(params[:id])
+        allUserJobs = User.all.find(params[:user_id]).jobs
+        render json: allUserJobs
+
     end
     
     private
     
     def job_params
-        params.require(:job).permit(:user_id, :usaJobs_job_id, :position_id, :job_title, :organization_name, :url, :department, :job_type, :schedule, :description, :requirement, :maximum_pay, :minimum_pay, :pay_period, :employer_strongpoints, :who_may_apply, :hiring_path, :location, :job_posting_date, :application_close_date, :applied_key, :favorite_key)
+        params.require(:job).permit(:id, :user_id, :usaJobs_job_id, :position_id, :job_title, :organization_name, :url, :department, :job_type, :schedule, :description, :requirement, :maximum_pay, :minimum_pay, :pay_period, :employer_strongpoints, :who_may_apply, :hiring_path, :location, :job_posting_date, :application_close_date, :applied_key, :favorite_key)
     end
 end
--- find_job_backend/app/controllers/notes_controller.rb
+++ find_job_backend/app/controllers/notes_controller.rb
@@ -1,39 +1,45 @@
 class NotesController < ApplicationController
     def index
-        notes = Note.all 
-        render json: notes
+      notes = Note.all 
+      render json: notes
     end
     
     def show
-        note = Note.find(params[:id])
-        render json: note
+      note = Note.find(params[:id])
+      render json: note
     end
     
     def create
-        note = Note.create(note_params)
-        if note.valid?
-          render json: note
-        else
-          render json:{errors: note.errors.full_messages}
-        end
+      note = Note.create(note_params)
+      userNotes = User.find(params[:user_id]).notes
+      if note.valid?
+        render json: {userNotes: userNotes}
+      else
+        render json:{errors: note.errors.full_messages}
+      end
     end
     
     def update
-        note = Note.find(params[:id])
-        if note.update(note_params)
-          render json: note
-        else
-          render json: note.errors, status: :unprocessable_entity
-        end
+      byebug
+      note = Note.find(params[:id])
+      userNotes = User.find(params[:user_id]).notes
+      if note.update(note_params)
+        render json: {userNotes: userNotes}
+      else
+        render json: note.errors, status: :unprocessable_entity
+      end
     end
     
     def destroy
+      # byebug
         Note.destroy(params[:id])
+        userNotes = User.find(params[:user_id]).notes
+        render json: {userNotes: userNotes}
     end
     
     private
     
     def note_params
-        params.require(:note).permit(:job_id, :text)
+        params.require(:note).permit(:job_id, :text, :user_id)
     end
 end
